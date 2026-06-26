a7a0e712b8a99afc912a586a77b6cc33d8ce9942
yura_sh <ariez_1@ukr.net>
2026-06-26 19:11:17 +0300

    add sql init script (raw , dwh layer)

--- sql/create_dwh_layer.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE crypto_statistics (
-    symbol VARCHAR(20),
-    avg_price NUMERIC,
-    min_price NUMERIC,
-    max_price NUMERIC,
-    updated_at TIMESTAMP DEFAULT NOW()
-);
--- sql/create_row_layer_script.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE warehouse.bitcoin_prices (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP,
-    price_usd NUMERIC
-);
-
